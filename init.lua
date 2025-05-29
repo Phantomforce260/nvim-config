698c9ed227d11469a603a0b9d5fb569a77620e63
Adri <aakeem0920@gmail.com>
2025-05-28 20:53:32 -0400

    QoL stuff for neo-tree

--- init.lua
+++ init.lua
@@ -1,6 +1,16 @@
-
 vim.g.mapleader = ";" 
-vim.keymap.set("n", "<leader>q", "<CMD>Oil<CR>");
+--vim.keymap.set("n", "<leader>q", "<CMD>Oil<CR>")
+vim.keymap.set("n", "<leader>w", "<C-w>l")
+
+vim.keymap.set("n", "<leader>q", function()
+    local ft = vim.bo.filetype
+
+    if ft ~= "oil" then
+        vim.cmd("Neotree close")
+    end
+
+    vim.cmd("Oil")
+end, { desc = "Escape to Oil and hide Neo-tree" })
 
 -- Synchronizes the system clipboard with Neovim's clipboard
 vim.opt.clipboard = "unnamedplus"
@@ -20,18 +30,107 @@ vim.opt.virtualedit = "block"
 -- See commands in nvim/lua/vim-optiond.lua
 require("vim-options")
 
+vim.api.nvim_create_augroup("OnFileOpened", { clear = true })
+
+vim.api.nvim_create_autocmd("VimEnter", {
+    group = "OnFileOpened",
+    callback = function()
+        local args = vim.fn.argv()
+        if #args == 1 then
+            local stat = vim.loop.fs_stat(args[1])
+            if stat and stat.type == "file" then
+                vim.cmd("Neotree filesystem reveal left")
+            end
+        end
+    end,
+})
+
+vim.api.nvim_create_augroup("OnOilOpenedFile", { clear = true })
+
+vim.api.nvim_create_autocmd("BufEnter", {
+    group = "OnOilOpenedFile",
+    callback = function()
+        local current = vim.api.nvim_buf_get_name(0)
+        local stat = vim.loop.fs_stat(current)
+        local focused = false
+
+        if stat and stat.type == "file" then
+            -- Check if previous buffer was Oil
+            local previous_buf = vim.fn.bufnr("#")
+            if previous_buf > 0 and vim.api.nvim_buf_is_valid(previous_buf) then
+                local ft = vim.api.nvim_buf_get_option(previous_buf, "filetype")
+                if ft == "oil" then
+                    vim.schedule(function()
+                        vim.cmd("Neotree filesystem reveal left")
+                    end)
+                end
+            end
+        end
+    end
+})
+
+vim.api.nvim_set_keymap('n', '<leader>w', '', {
+  noremap = true,
+  silent = true,
+  callback = function()
+    local buf = vim.api.nvim_get_current_buf()
+    local buf_ft = vim.api.nvim_buf_get_option(buf, 'filetype')
+
+    -- Neo-tree buffers usually have filetype 'neo-tree'
+    if buf_ft == 'neo-tree' then
+      -- Neo-tree is focused, switch to previous window
+      vim.cmd('wincmd p')
+    else
+      -- Focus Neo-tree window if it exists, otherwise open it
+      local found_neo_tree_win = nil
+      for _, win in ipairs(vim.api.nvim_list_wins()) do
+        local win_buf = vim.api.nvim_win_get_buf(win)
+        local win_buf_ft = vim.api.nvim_buf_get_option(win_buf, 'filetype')
+        if win_buf_ft == 'neo-tree' then
+          found_neo_tree_win = win
+          break
+        end
+      end
+
+      if found_neo_tree_win then
+        vim.api.nvim_set_current_win(found_neo_tree_win)
+      end
+    end
+  end
+})
+
+vim.api.nvim_create_user_command('Q', function()
+  -- Close all neo-tree windows
+  for _, win in ipairs(vim.api.nvim_list_wins()) do
+    local buf = vim.api.nvim_win_get_buf(win)
+    local ft = vim.api.nvim_buf_get_option(buf, 'filetype')
+    if ft == 'neo-tree' then
+      vim.api.nvim_win_close(win, true)
+    end
+  end
+  -- Then quit current window
+  vim.cmd('q')
+end, {})
+
+--vim.api.nvim_create_user_command('Q', close_neo_tree_and_quit, {})
+--vim.cmd([[command! -bang -nargs=* q execute 'Q']])
+
+vim.api.nvim_create_user_command('W', function()
+    vim.cmd('w')
+end, {})
+
 -- Set up lazy.nvim -----------------------------------
 
 local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
 if not (vim.uv or vim.loop).fs_stat(lazypath) then
-  vim.fn.system({
-    "git",
-    "clone",
-    "--filter=blob:none",
-    "https://github.com/folke/lazy.nvim.git",
-    "--branch=stable", -- latest stable release
-    lazypath,
-  })
+    vim.fn.system({
+        "git",
+        "clone",
+        "--filter=blob:none",
+        "https://github.com/folke/lazy.nvim.git",
+        "--branch=stable", -- latest stable release
+        lazypath,
+    })
 end
 vim.opt.rtp:prepend(lazypath)
 
