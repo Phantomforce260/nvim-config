cd90617c6f1129ee74d363b34393cb613c9da428
Adri <aakeem0920@gmail.com>
2025-10-28 13:04:09 -0400

    Upgrading to nvim 0.11

--- lua/lsp.lua
+++ lua/lsp.lua
@@ -32,8 +32,7 @@ return {
                 },
             }
 
-            require("mason-lspconfig").setup({})
-            require("mason-lspconfig").setup_handlers({
+            require("mason-lspconfig").setup({
                 function(server_name)
                     require("lspconfig")[server_name].setup({
                         capabilities = capabilities,
